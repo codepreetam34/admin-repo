4f55affd3d2a658481e06edc1c10d7b4eb5b8a71
codepreetam34 <preetamkumar3232@mail.com>
2024-02-08 12:56:40 +0530

    added

--- src/Components/DisplayPagesContainer/Components/PagesContainer.jsx
+++ src/Components/DisplayPagesContainer/Components/PagesContainer.jsx
@@ -2,17 +2,20 @@ import React from "react";
 import { Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-// Import the images
+// Import the images:
+// ------------------
 //import Greenbox from "../../../../src/images/greenbox.png";
 //import Redbox from "../../../../src/images/redbox.png";
 //import Orangebox from "../../../../src/images/orangebox.png";
 //import Bluebox from "../../../../src/images/bluebox.png";
 
 const PagesContainer = () => {
+
   const Greenbox = '<i class="fa-solid fa-house-circle-check fa-2xl" style="color: #00ada3;"></i>';
   const Redbox = '<i class="fa-solid fa-clipboard-list fa-2xl" style="color: #801317;"></i>';
   const Orangebox = '<i class="fa-brands fa-product-hunt fa-2xl" style="color: #ff9f5e;"></i>';
   const Bluebox = '<i class="fa-solid fa-cart-plus fa-2xl" style="color: #31456c;"></i>'
+
   const pageInfo = [
     {
       boxHref: "/display-pages/homepage-display",
